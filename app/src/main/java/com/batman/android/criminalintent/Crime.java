6c5f88cde73b899b3875ce3e9fab40eed3957eb0
Roman Artemenko <RGArtemenko@gmail.com>
2016-09-30 23:42:13 +0300

    added new views

--- app/src/main/java/com/batman/android/criminalintent/Crime.java
+++ app/src/main/java/com/batman/android/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.batman.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,29 @@ public class Crime {
     private UUID mId;
     private String mTitle;
 
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    private Date mDate;
+    private boolean mSolved;
+
     public Crime() {
         //Генерирование уникального индентификатора
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
